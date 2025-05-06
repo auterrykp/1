bee862ba31068a6153a690a06ff2fc53eef87488
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-05-07 07:24:16 +0800

    Update font.master.loader.user.js

--- font.master.loader.user.js
+++ font.master.loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Font.master.loader(user.script)
 // @namespace    Font.master.loader(user.script)
-// @version      0.1.4
+// @version      0.2
 // @description  Font.master.loader(user.script)
 // @author       auterrykp
 // @license      N/A
@@ -21,7 +21,7 @@ const allowedDomains = [
 'shoppingdesign.com.tw','betterme-magazine.com','adaymag.com',
 'tokyocheapo.com','harpersbazaar.com','vogue.com.tw','oasistrek.com',
 'jobsdb.com', 'indeed.com','ctgoodjobs.hk','sc.com',
-'bbc.com','nytimes.com','wikipedia.org','cips.org',
+'bbc.com','nytimes.com','wikipedia.org','cips.org','www3.nhk.or.jp',
 '500px.com','flickr.com','sony.com','apple.com'
 
 ];
