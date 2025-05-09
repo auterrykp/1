f71765acf5acfdf88d9a1943a119672dbc2e1e7c
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-05-10 06:34:05 +0800

    Update font.master.loader.user.js

--- font.master.loader.user.js
+++ font.master.loader.user.js
@@ -45,7 +45,7 @@ const allowedDomains = [
   --main-font: 'CathaySans_Lt';
   --jp-font: 'Hiragino Kaku Gothic ProN';
   --alt-font: 'CathaySans_Lt','STXihei-Light','Noto Sans HK';
-  --serif-font: 'EB Garamond', 'Hiragino Mincho ProN';
+  --serif-font: 'Hiragino Mincho ProN';
 }
 
 
