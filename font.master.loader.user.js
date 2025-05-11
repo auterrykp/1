bc00bee9f4a000545a0db747fe7ac7ea7671b01f
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-05-11 17:33:47 +0800

    Update font.master.loader.user.js

--- font.master.loader.user.js
+++ font.master.loader.user.js
@@ -7,6 +7,8 @@
 // @license      N/A
 // @match        *://*/*
 // @exclude      https://github.*/*
+// @exclude      https://linkedin.*/*
+// @exclude      https://m.facebook.com/*
 // @grant        none
 // @run-at       document-start
 // @downloadURL  https://github.com/auterrykp/1/raw/main/font.master.loader.user.js
@@ -205,7 +207,7 @@ html[xmlns="http://www.w3.org/1999/xhtml"],[lang="zh-Hant"] {
   box-sizing: border-box !important;
 }
 
-/*hk01,mingpao,am730,betterme,tokyoceapo*/
+/*hk01,mingpao,am730*,betterme,tokyoceapo/
 .incontent p,
 dfn+div p,
 p.break-words,
@@ -216,111 +218,26 @@ article.post-wrap .single-entry p {
 }
 
 
-
-/*Hide elements*/
-
-/* Grouped IDs */
-#AD-970x250-AD1,
-#abtest_banner_li,
-#ad-fullscreen,
-#ad-lite-recommend-wrap,
-#ad-overlay,
-#ad_banner_pos1,
-#ad_unit,
-#adsDefinitions,
-#afscontainer1,
-#banner-close-monkey,
-#banner-exit,
-#branch-banner-iframe,
-#contentSponsorshipAd,
-#cto_banner_content,
-#google-center-div,
-#google_ads_frame1,
-#google_ads_iframe,
-#google_center_div,
-#google_image_div,
-#gwd-ad,
-#mobile_banner_native_1,
-#nativead_0,
-#pixad891d,
-#post_under_ads,
-#sda-LREC2,
-#sda-WFPAD,
-#topInAppNotificationBanner,
-#zi_ad_article_below_title,
-#footerBanner,
-#footerPromo,
-.AdContainer-sc-8mk8fx-0,
-.RichSmartAppBanner,
-._popIn_recommend_container,
-.ad-box,
-.ad-flyingcarpet1-placeholder,
-.ad-frame-fix,
-.ad-tip,
-.ad300,
-.adContainer,
-.adWrap,
-.ad_google_LREC,
-.adncon,
-.adnol,
-.ads-parallax-fixed,
-.adsbyfalcon,
-.adsbygoogle,
-.advert__holder,
-.banner,
-.bnextmeida-banner,
-.footerBanner,
-.footerPromo,
-.googleAds-frame,
-.google_ads_iframes,
-.gpt_ads_nobg,
-.gpt_ads_title,
-.header-ad,
-.herald-ad,
-.img_ad,
-.inread-ad-container,
-.kiwi-ad-sticky-wrapper,
-.kiwi-ad-wrapper-ypa-300x250,
-.mpads,
-.native_ad,
-.new_ad,
-.pixad2199d,
-.share-btns,
-.share-widget,
-.shareSocalBtn,
-.share_btn,
-.smart-banner-container,
-.smartbanner,
-.sp_foreign_banner,
-.stick-share,
-.subscribe-bar-container,
-.subscribe-cover,
-.tmb_tn_ad_content,
-.tm-floating-banner__contents,
-.top-promo,
-.zucks-widget-container,
-.incontent .ssm,
-.placeholder-ad,
-.mt-4,
-.ANDBEYOND-inread,
-.teads-inread,
-.infogram-embed,
-.bg-light-n2,
-#teads0,
-.fly123,
-.cmp-lcs-stack,
-#posNM,
-.posNM,
-[id*="adbox--page_auto_ad"],
-.adbox--page_partner_ad
-
- {
+/* Grouped IDs and Classes for Ad/Share/Promo Elements */
+#AD-970x250-AD1, #abtest_banner_li, #ad-fullscreen, #ad-lite-recommend-wrap, #ad-overlay, #ad_banner_pos1, #ad_unit,
+#adsDefinitions, #afscontainer1, #banner-close-monkey, #banner-exit, #branch-banner-iframe, #contentSponsorshipAd,
+#cto_banner_content, #google-center-div, #google_ads_frame1, #google_ads_iframe, #google_center_div, #google_image_div,
+#gwd-ad, #mobile_banner_native_1, #nativead_0, #pixad891d, #post_under_ads, #sda-LREC2, #sda-WFPAD,
+#topInAppNotificationBanner, #zi_ad_article_below_title, #footerBanner, #footerPromo, #teads0, #posNM,
+[id*="adbox--page_auto_ad"], .AdContainer-sc-8mk8fx-0, .RichSmartAppBanner, ._popIn_recommend_container, .ad-box,
+.ad-flyingcarpet1-placeholder, .ad-frame-fix, .ad-tip, .ad300, .adContainer, .adWrap, .ad_google_LREC, .adncon,
+.adnol, .ads-parallax-fixed, .adsbyfalcon, .adsbygoogle, .advert__holder, .banner, .bnextmeida-banner, .footerBanner,
+.footerPromo, .googleAds-frame, .google_ads_iframes, .gpt_ads_nobg, .gpt_ads_title, .header-ad, .herald-ad, .img_ad,
+.inread-ad-container, .kiwi-ad-sticky-wrapper, .kiwi-ad-wrapper-ypa-300x250, .mpads, .native_ad, .new_ad, .pixad2199d,
+.share-btns, .share-widget, .shareSocalBtn, .share_btn, .smart-banner-container, .smartbanner, .sp_foreign_banner,
+.stick-share, .subscribe-bar-container, .subscribe-cover, .tmb_tn_ad_content, .tm-floating-banner__contents,
+.top-promo, .zucks-widget-container, .incontent .ssm, .placeholder-ad, .mt-4, .ANDBEYOND-inread, .teads-inread,
+.infogram-embed, .bg-light-n2, .fly123, .cmp-lcs-stack, .posNM {
 
     display: none !important;
     height: 0px !important;
 
 }
-
 
   `;
   document.head.appendChild(style);
