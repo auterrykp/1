0770462d53ce21dc4bb0105dbd000812ffafc377
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-05-14 05:49:41 +0800

    Update font.master.loader.user.js

--- font.master.loader.user.js
+++ font.master.loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Font.master.loader(user.script)
 // @namespace    Font.master.loader(user.script)
-// @version      0.4.4
+// @version      0.4.5
 // @description  Font.master.loader(user.script)
 // @author       auterrykp
 // @license      N/A
