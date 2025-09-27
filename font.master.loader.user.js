fb6da890e5a03e834eec7b395443f30ea148ed15
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-09-27 08:12:27 +0800

    Update font.master.loader.user.js

--- font.master.loader.user.js
+++ font.master.loader.user.js
@@ -7,6 +7,7 @@
 // @license      N/A
 // @match        *://*/*
 // @exclude      https://github.*/*
+// @exclude      https://account.microsoft.*/
 // @exclude      https://fonts.google.com/*
 // @exclude      https://*highwaybus.co*/*
 // @grant        none
